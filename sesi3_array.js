1a832741da5893a0d2254fbce0a2f27f300ffa57
GITFA16 <alamudifaiza@hotmail.com>
2026-08-10 10:22:20 +0200

    initial commit- PR Sesi 3

---
+++ sesi3_array.js
@@ -0,0 +1,12 @@
+let umur = [20, 25, 30, 35, 40];
+umur.push(45);
+let buah = ["apel", "jeruk", "pisang", "mangga", "anggur"];
+
+// buah.pop();
+// buah.shift();
+buah.splice(1, 2, "kiwi", "nanas");
+// let campuran = [10, "apel", {true: true, umur: 25.5, status: null}];
+// console.log(campuran[2].umur); 
+// console.log(umur);
+console.log(buah);
+// console.log(campuran);
