8b2d787f5d87d8836d66e16311a3c0ca06e7da24
GITFA16 <alamudifaiza@hotmail.com>
2026-08-10 10:37:54 +0200

    case for git revert

--- sesi4_mainFA.js
+++ sesi4_mainFA.js
@@ -4,3 +4,7 @@ console.log("Penjumlahan:", kalkulator(27, 13, "+"));
 console.log("Pengurangan:", kalkulator(84, 29, "-"));
 console.log("Perkalian:", kalkulator(12, 7, "*"));
 console.log("Pembagian:", kalkulator(96, 8, "/"));
+
+//latihan getRevert
+console.log("latihan getRevert");
+
