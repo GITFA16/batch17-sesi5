a497f805c9663c7303d277cc4be6ec3fc9ad74b8
GITFA16 <alamudifaiza@hotmail.com>
2026-08-10 10:29:12 +0200

    PR sesi 4 main.js

---
+++ sesi4_mainFA.js
@@ -0,0 +1,6 @@
+import { kalkulator } from "./kalkulatorFA.js";
+
+console.log("Penjumlahan:", kalkulator(27, 13, "+"));
+console.log("Pengurangan:", kalkulator(84, 29, "-"));
+console.log("Perkalian:", kalkulator(12, 7, "*"));
+console.log("Pembagian:", kalkulator(96, 8, "/"));
