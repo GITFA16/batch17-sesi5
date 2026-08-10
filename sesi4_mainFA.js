553e37524fca7962cf434e58bba1c44085802937
GITFA16 <alamudifaiza@hotmail.com>
2026-08-10 10:53:33 +0200

    revert case second try

--- sesi4_mainFA.js
+++ sesi4_mainFA.js
@@ -4,3 +4,5 @@ console.log("Penjumlahan:", kalkulator(27, 13, "+"));
 console.log("Pengurangan:", kalkulator(84, 29, "-"));
 console.log("Perkalian:", kalkulator(12, 7, "*"));
 console.log("Pembagian:", kalkulator(96, 8, "/"));
+
+console.log("tes revert lagi");
